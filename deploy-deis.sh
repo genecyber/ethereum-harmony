885370b4707d1a4d29f059acbb5b46af82713aba
Shannon Code <genecyber@gmail.com>
2017-03-13 14:17:20 -0400

    m

--- deploy-deis.sh
+++ deploy-deis.sh
@@ -1,4 +1,4 @@
-git add --all
-git commit -m "Dockerized Deis Deploy"
-deis healthchecks:set readiness httpGet 8080 --timeout 500 --initial 100
+#git add --all
+#git commit -m "Dockerized Deis Deploy"
+#deis healthchecks:set readiness httpGet 8080 --timeout 500 --initial 100
 git push deis
